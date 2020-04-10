04e2d2635a53f72ebde4ae1620a29169d2c519f5
Jeyabalaji <balaji.jeya@gmail.com>
2020-04-11 00:40:47 +0800

    Initial commit

---
+++ app/src/main/java/com/antandbuffalo/birthdayreminder/DriveServiceHelper.java
@@ -0,0 +1,75 @@
+package com.antandbuffalo.birthdayreminder;
+
+import android.util.Pair;
+
+import com.google.android.gms.auth.UserRecoverableAuthException;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class DriveServiceHelper {
+    private final Executor mExecutor = Executors.newSingleThreadExecutor();
+    private final Drive mDriveService;
+
+    public DriveServiceHelper(Drive driveService) {
+        mDriveService = driveService;
+    }
+
+    /**
+     * Creates a text file in the user's My Drive folder and returns its file ID.
+     */
+    public Task<String> createFile() {
+        return Tasks.call(mExecutor, () -> {
+            File metadata = new File()
+                    .setParents(Collections.singletonList("appDataFolder"))
+                    .setMimeType("text/plain")
+                    .setName("config.json");
+
+            File googleFile = mDriveService.files().create(metadata).execute();
+            if (googleFile == null) {
+                throw new IOException("Null result when requesting file creation.");
+            }
+
+            return googleFile.getId();
+        });
+    }
+
+    /**
+     * Opens the file identified by {@code fileId} and returns a {@link Pair} of its name and
+     * contents.
+     */
+    public Task<Pair<String, String>> readFile(String fileId) {
+        return Tasks.call(mExecutor, () -> {
+            // Retrieve the metadata as a File object.
+            File metadata = mDriveService.files().get(fileId).execute();
+            String name = metadata.getName();
+
+            // Stream the file contents to a String.
+            try (InputStream is = mDriveService.files().get(fileId).executeMediaAsInputStream();
+                 BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+                String contents = stringBuilder.toString();
+
+                return Pair.create(name, contents);
+            }
+        });
+    }
+
+    //https://github.com/mesadhan/google-drive-app
+
+}
---
+++ app/src/main/java/com/antandbuffalo/birthdayreminder/MainActivity.java
@@ -0,0 +1,178 @@
+package com.antandbuffalo.birthdayreminder;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RestrictTo;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.Collections;
+
+public class MainActivity extends AppCompatActivity {
+
+    DriveServiceHelper driveServiceHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                createFile();
+            }
+        });
+
+        driveSignIn();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void driveSignIn() {
+        // https://stackoverflow.com/questions/54052220/how-to-access-the-application-data-on-google-drive-on-android-with-its-rest-api
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                //.requestIdToken("405608185466-1ertijbnjfc99cgdvbmplv7r0c840vu6.apps.googleusercontent.com")x
+                .requestEmail()
+                .requestScopes(new Scope(Scopes.DRIVE_APPFOLDER))
+                .build();
+        GoogleSignInClient googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+        startActivityForResult(googleSignInClient.getSignInIntent(), 999);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 999) {
+            handleSignIn(data);
+        }
+    }
+
+    public void handleSignIn(Intent data) {
+        GoogleSignIn.getSignedInAccountFromIntent(data)
+                .addOnSuccessListener(new OnSuccessListener<GoogleSignInAccount>() {
+                    @Override
+                    public void onSuccess(GoogleSignInAccount googleSignInAccount) {
+                        Log.d("JBL", "Signed in as " + googleSignInAccount.getEmail());
+
+                        // Use the authenticated account to sign in to the Drive service.
+                        GoogleAccountCredential credential =
+                                GoogleAccountCredential.usingOAuth2(
+                                        getApplicationContext(), Collections.singleton(DriveScopes.DRIVE_APPDATA));
+                        credential.setSelectedAccount(googleSignInAccount.getAccount());
+                        Drive googleDriveService =
+                                new Drive.Builder(
+                                        AndroidHttp.newCompatibleTransport(),
+                                        new GsonFactory(),
+                                        credential)
+                                        .setApplicationName("Birthday Reminder V2")
+                                        .build();
+                        driveServiceHelper = new DriveServiceHelper(googleDriveService);
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i("FAIL", e.getLocalizedMessage());
+                    }
+                });
+    }
+
+    /**
+     * Creates a new file via the Drive REST API.
+     */
+    private void createFile() {
+        if (driveServiceHelper != null) {
+            Log.d("JBL", "Creating a file.");
+
+            driveServiceHelper.createFile()
+                    .addOnSuccessListener(fileId -> readFile(fileId))
+                    .addOnFailureListener(exception ->
+                            Log.e("JBL", "Couldn't create file.", exception));
+        }
+    }
+
+    /**
+     * Retrieves the title and content of a file identified by {@code fileId} and populates the UI.
+     */
+    private void readFile(String fileId) {
+        if (driveServiceHelper != null) {
+            Log.d("JBL", "Reading file " + fileId);
+
+            driveServiceHelper.readFile(fileId)
+                    .addOnSuccessListener(nameAndContent -> {
+                        String name = nameAndContent.first;
+                        String content = nameAndContent.second;
+
+                        Log.d("JBL", "File name: " + name);
+                        Log.d("JBL", "File content: " + content);
+                    })
+                    .addOnFailureListener(exception ->
+                            Log.e("JBL", "Couldn't read file.", exception));
+        }
+    }
+    public void uploadFile() {
+        File fileMetadata = new File();
+        fileMetadata.setName("config.json");
+        fileMetadata.setParents(Collections.singletonList("appDataFolder"));
+        java.io.File filePath = new java.io.File("files/config.json");
+        FileContent mediaContent = new FileContent("application/json", filePath);
+//        File file = driveService.files().create(fileMetadata, mediaContent)
+//                .setFields("id")
+//                .execute();
+//        System.out.println("File ID: " + file.getId());
+    }
+}
