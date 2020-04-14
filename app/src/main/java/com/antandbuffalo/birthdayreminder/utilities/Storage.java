e6aa587b113a015ae724af565602bafde8b2d881
Jeyabalaji <balaji.jeya@gmail.com>
2020-04-15 01:48:19 +0800

    using preferences instead of table

--- app/src/main/java/com/antandbuffalo/birthdayreminder/utilities/Storage.java
+++ app/src/main/java/com/antandbuffalo/birthdayreminder/utilities/Storage.java
@@ -2,8 +2,6 @@ package com.antandbuffalo.birthdayreminder.utilities;
 
 import android.content.SharedPreferences;
 
-import com.antandbuffalo.birthdayreminder.utilities.Constants;
-
 import java.util.Date;
 
 /**
@@ -25,21 +23,63 @@ public class Storage {
         editor.commit();
     }
 
+    public static String getRestoreTime(SharedPreferences preferences, String key) {
+        return Storage.getString(preferences, key, new Date().toString());
+    }
+    public static Boolean setRestoreTime(SharedPreferences preferences, String key, String dateTime) {
+        return Storage.putString(preferences, key, dateTime);
+    }
+
+    public static String getBackupTime(SharedPreferences preferences, String key) {
+        return Storage.getString(preferences, key, new Date().toString());
+    }
+    public static Boolean setBackupTime(SharedPreferences preferences, String key, String dateTime) {
+        return Storage.putString(preferences, key, dateTime);
+    }
+
+    public static Integer getNotificationPerDay(SharedPreferences preferences, String key) {
+        return Storage.getInt(preferences, key, 1);
+    }
+    public static Boolean setNotificationPerDay(SharedPreferences preferences, String key, Integer days) {
+        return Storage.putInt(preferences, key, days);
+    }
+
     public static Integer getNotificationHours(SharedPreferences preferences) {
-        return preferences.getInt(Constants.PREFERENCE_NOTIFICATION_TIME_HOURS, 0);
+        return Storage.getInt(preferences, Constants.PREFERENCE_NOTIFICATION_TIME_HOURS);
     }
-
     public static Integer getNotificationMinutes(SharedPreferences preferences) {
-        return preferences.getInt(Constants.PREFERENCE_NOTIFICATION_TIME_MINUTES, 0);
+        return Storage.getInt(preferences, Constants.PREFERENCE_NOTIFICATION_TIME_MINUTES);
     }
 
-    public static String getBackupLoadedTime(SharedPreferences preferences, String key) {
-        return preferences.getString(key, new Date().toString());
+    public static Integer getPreNotificationDays(SharedPreferences preferences, String key) {
+        return Storage.getInt(preferences, key);
+    }
+    public static Boolean setPreNotificationDays(SharedPreferences preferences, String key, Integer days) {
+        return Storage.putInt(preferences, key, days);
     }
 
-    public static void setBackupLoadedTime(SharedPreferences preferences, String key, String dateTime) {
+    public static Boolean putString(SharedPreferences preferences, String key, String value) {
         SharedPreferences.Editor editor = preferences.edit();
-        editor.putString(key, dateTime);
-        editor.commit();
+        editor.putString(key, value);
+        return editor.commit();
     }
+    public static String getString(SharedPreferences preferences, String key) {
+        return Storage.getString(preferences, key, "");
+    }
+    public static String getString(SharedPreferences preferences, String key, String defaultValue) {
+        return preferences.getString(key, defaultValue);
+    }
+
+    public static Boolean putInt(SharedPreferences preferences, String key, Integer value) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt(key, value);
+        return editor.commit();
+    }
+    public static Integer getInt(SharedPreferences preferences, String key) {
+        return Storage.getInt(preferences, key, 0);
+    }
+    public static Integer getInt(SharedPreferences preferences, String key, Integer defaultValue) {
+        return preferences.getInt(key, defaultValue);
+    }
+
 }
