0b96a72fb33cae678b97c1622a79af3b6176cbf7
Jeyabalaji <balaji.jeya@gmail.com>
2020-06-30 17:09:22 +0800

    Theme option added

--- app/src/main/java/com/antandbuffalo/birthdayreminder/utilities/ThemeOptions.java
+++ app/src/main/java/com/antandbuffalo/birthdayreminder/utilities/ThemeOptions.java
@@ -8,20 +8,23 @@ import java.util.Map;
 public class ThemeOptions {
     private static ThemeOptions themeOptions = null;
     private static List<Map<String, String>> values;
+    public static String KEY_DEFAULT = "default";
+    public static String KEY_LIGHT = "light";
+    public static String KEY_DARK = "dark";
     private ThemeOptions() {
         values = new ArrayList<>();
         Map<String, String> item = new HashMap<>();
-        item.put("key", "default");
+        item.put("key", KEY_DEFAULT);
         item.put("value", "System Default");
         values.add(item);
 
         item = new HashMap<>();
-        item.put("key", "light");
+        item.put("key", KEY_LIGHT);
         item.put("value", "Light");
         values.add(item);
 
         item = new HashMap<>();
-        item.put("key", "dark");
+        item.put("key", KEY_DARK);
         item.put("value", "Dark");
         values.add(item);
     }
