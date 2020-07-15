532713bc63c518e1c5620286f8c2ea05b6bc0be3
Jeyabalaji <balaji.jeya@gmail.com>
2020-07-15 18:47:53 +0800

    auto restore logic changed

--- app/src/main/java/com/antandbuffalo/birthdayreminder/utilities/FirebaseHandler.java
+++ app/src/main/java/com/antandbuffalo/birthdayreminder/utilities/FirebaseHandler.java
@@ -7,4 +7,5 @@ public interface FirebaseHandler {
     default public void onCompleteDateOfBirthSync(Task<DocumentSnapshot> task) {};
     default public void onCompleteUserPreferenceSync(Task<DocumentSnapshot> task) {};
     default public void onCompleteDateOfBirthUserPreferenceSync(Task<DocumentSnapshot> dobTask, Task<DocumentSnapshot> preferenceTask) {};
+    default public void onCompleteDateOfBirthUserPreferenceSync(String type, Task<DocumentSnapshot> task) {};
 }
