f73e7d538ec24982420cf4e6a85b5b8d58d701b3
Jeyabalaji <jeyabalaji.tm@grabtaxi.com>
2023-03-06 20:32:05 +0530

    moved add button

---
+++ app/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Birthday Reminder
+        <br />
+        <img className='app_icon' src='app_icon.png' />
+        Login with Google Account
+      </header>
+    </div>
+  );
+}
+
+export default App;
